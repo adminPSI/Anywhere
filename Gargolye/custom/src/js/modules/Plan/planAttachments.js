8c76255e5c2a9d3770e32ec0d04c8cb6dfcf409d
Ash Bloomingdale <ash.bloomingdale@primarysolutions.net>
2022-10-11 13:03:33 -0400

    attachment ID fix

--- Gargolye/custom/src/js/modules/Plan/planAttachments.js
+++ Gargolye/custom/src/js/modules/Plan/planAttachments.js
@@ -1,10 +1,8 @@
 const planAttachment = (() => {
-
   let attachments;
 
   /** Class for attachments in Plan Module */
   class PlanAttachment {
-
     /**
      * Create a new attachment instance
      * @param {[string]} header Header for Attachment Popup
@@ -12,15 +10,15 @@ const planAttachment = (() => {
      * @param {string} assessmentId Assessment ID
      */
     constructor(header, questionId, assessmentId) {
-      this.attachmentsForQuestion = []
+      this.attachmentsForQuestion = [];
       this.header = header;
       this.questionId = questionId;
       this.assessmentId = assessmentId;
     }
 
-    buildAttachmentButton () {
-
-      if (attachments.has(this.questionId)) this.attachmentsForQuestion = attachments.get(this.questionId);
+    buildAttachmentButton() {
+      if (attachments.has(this.questionId))
+        this.attachmentsForQuestion = attachments.get(this.questionId);
 
       const attachmentButton = button.build({
         id: `attach-${this.questionId}`,
@@ -28,14 +26,14 @@ const planAttachment = (() => {
         style: 'secondary',
         type: 'contained',
         callback: () => {
-          this.attachmentPopup()
-        }
+          this.attachmentPopup();
+        },
       });
 
       attachmentButton.classList.add('attachmentBtn');
       if (this.attachmentsForQuestion.length > 0) attachmentButton.classList.add('hasAttachments');
 
-      return attachmentButton
+      return attachmentButton;
     }
 
     attachmentPopup() {
@@ -48,97 +46,96 @@ const planAttachment = (() => {
       let ro;
       const planStatus = plan.getPlanStatus();
       const planActiveStatus = plan.getPlanActiveStatus();
-  
+
       if (planActiveStatus && planStatus === 'D' && $.session.planUpdate) {
         ro = false;
       } else {
         ro = true;
-      }      
+      }
       // =========================
       // UTIL FUNCTIONS
       // =============================
       // Adds attachments to the existing attachments section in popup
       function populateExistingAttachments() {
-        const header = document.createElement("h5");
-        header.innerText = "Existing Attachments:";
+        const header = document.createElement('h5');
+        header.innerText = 'Existing Attachments:';
         reviewAttachmentList.appendChild(header);
 
         attachmentsForQuestion.forEach(attachment => {
-          const fileContainer = document.createElement("div");
-          fileContainer.classList.add("reviewAttachmentContainer");
-          fileContainer.setAttribute('id', attachment.attachmentId)
-          fileContainer.setAttribute('delete', false)
+          const fileContainer = document.createElement('div');
+          fileContainer.classList.add('reviewAttachmentContainer');
+          fileContainer.setAttribute('id', attachment.planAttachmentId);
+          fileContainer.setAttribute('delete', false);
           const removeAttachmentBtn = button.build({
-            style: "secondary",
-            type: "text",
-            icon: "delete",
-            callback: event => addRemoveAttachmentToDeleteList(event.target.parentElement)
+            style: 'secondary',
+            type: 'text',
+            icon: 'delete',
+            callback: event => addRemoveAttachmentToDeleteList(event.target.parentElement),
           });
-          const file = document.createElement("p");
+          const file = document.createElement('p');
           file.innerText = attachment.description;
           if (!ro) fileContainer.appendChild(removeAttachmentBtn);
           fileContainer.appendChild(file);
           file.addEventListener('click', event => {
-            const attachmentId  = event.target.parentElement.id;
-            planAjax.viewPlanAttachment(attachmentId, '')
-          })
+            const attachmentId = event.target.parentElement.id;
+            planAjax.viewPlanAttachment(attachmentId, '');
+          });
           reviewAttachmentList.appendChild(fileContainer);
-        })
-      };
+        });
+      }
       // CB for clicking the delete button on attachments in existing attachment section
       function addRemoveAttachmentToDeleteList(attachmentElement) {
         const currentDeleteStatus = attachmentElement.getAttribute('delete');
         const attachmentId = attachmentElement.getAttribute('id');
         switch (currentDeleteStatus) {
           case 'false':
-            attachmentElement.classList.add('deleteAttachment')
+            attachmentElement.classList.add('deleteAttachment');
             attachmentsToDelete.push(attachmentId);
-            attachmentElement.setAttribute('delete', true)
+            attachmentElement.setAttribute('delete', true);
             break;
           case 'true':
-            attachmentElement.classList.remove('deleteAttachment')
-            attachmentsToDelete = attachmentsToDelete.filter(attachment => attachment !== attachmentId);
-            attachmentElement.setAttribute('delete', false)
+            attachmentElement.classList.remove('deleteAttachment');
+            attachmentsToDelete = attachmentsToDelete.filter(
+              attachment => attachment !== attachmentId,
+            );
+            attachmentElement.setAttribute('delete', false);
             break;
           default:
             break;
         }
-      };
+      }
       // checks file is a valid type
       function fileValidation(target) {
         const fileType = target.files[0].type;
-        const reFileTypeTest = new RegExp('(audio\/)|(video\/)')
+        const reFileTypeTest = new RegExp('(audio/)|(video/)');
         if (reFileTypeTest.test(fileType)) {
-          alert('Anywhere currently does not accept audio or video files')
+          alert('Anywhere currently does not accept audio or video files');
           target.value = '';
-          return false
+          return false;
         }
       }
       // CB for clicking the add attachment button. doesn't do anything if they haven't yet chosen an
       // attachment for existing add attachment input.
       function addNewAttachment() {
         function addRemoveAttachmentEventListener(btn) {
-          btn.addEventListener("click", event => {
-            const fileInput = event.target.parentElement.getElementsByTagName(
-              "input"
-            )[0];
-            if (fileInput !== "") {
+          btn.addEventListener('click', event => {
+            const fileInput = event.target.parentElement.getElementsByTagName('input')[0];
+            if (fileInput !== '') {
               event.target.parentElement.remove();
             }
           });
         }
         // Don't add new attachment input if the last attachment is empty
-        const form = document.getElementById("attachmentForm");
+        const form = document.getElementById('attachmentForm');
         const lastAttachment = form.lastChild;
-        if (
-          lastAttachment &&
-          lastAttachment.querySelector(".attachmentInput").value === ""
-        ) {
+        if (lastAttachment && lastAttachment.querySelector('.attachmentInput').value === '') {
           return;
         }
         const newFileContainer = attachmentContainer.cloneNode(true);
-        newFileContainer.getElementsByTagName("input")[0].value = ""; //remove file for new container
-        newFileContainer.getElementsByTagName("input")[0].addEventListener('change', evt => fileValidation(evt.target))
+        newFileContainer.getElementsByTagName('input')[0].value = ''; //remove file for new container
+        newFileContainer
+          .getElementsByTagName('input')[0]
+          .addEventListener('change', evt => fileValidation(evt.target));
         attachmentList.appendChild(newFileContainer);
         addRemoveAttachmentEventListener(newFileContainer.firstElementChild);
         attachmentContainer = newFileContainer;
@@ -153,47 +150,47 @@ const planAttachment = (() => {
           attachmentArray.forEach(attachment => {
             const saveProm = new Promise(resolve => {
               const saveData = {
-                  token: $.session.Token,
-                  assessmentId: assessmentId,
-                  description: attachment.description,
-                  attachmentType: attachment.type,
-                  attachment: attachment.arrayBuffer,
-                  section: '',
-                  questionId: questionId
-                }
-              planAjax.addPlanAttachment(saveData).then(res => resolve(res))
+                token: $.session.Token,
+                assessmentId: assessmentId,
+                description: attachment.description,
+                attachmentType: attachment.type,
+                attachment: attachment.arrayBuffer,
+                section: '',
+                questionId: questionId,
+              };
+              planAjax.addPlanAttachment(saveData).then(res => resolve(res));
             });
-            attachmentSaveArray.push(saveProm)
+            attachmentSaveArray.push(saveProm);
           });
 
-          const attSaveRes = await Promise.all(attachmentSaveArray)
+          const attSaveRes = await Promise.all(attachmentSaveArray);
           attSaveRes.forEach(att => {
-            attachmentsAdded.push(att[0])
+            attachmentsAdded.push(att[0]);
           });
         }
         // DELETE ATTACHMENTS
-          attachmentsToDelete.forEach(attachment => {
-            planAjax.deletePlanAttachment(assessmentId, attachment);
-          });
+        attachmentsToDelete.forEach(attachment => {
+          planAjax.deletePlanAttachment(assessmentId, attachment);
+        });
 
         // ADD ATTACHMENTS
-        const attachmentInputs = document.querySelectorAll(".attachmentInput");
+        const attachmentInputs = document.querySelectorAll('.attachmentInput');
         if (attachmentInputs.length === 0) {
-          console.log("no attachments to add");
+          console.log('no attachments to add');
           return;
         }
-    
+
         let attachmentProms = [];
         let attachmentArray = [];
         attachmentInputs.forEach(inputElement => {
-          if (inputElement.value === "") {
+          if (inputElement.value === '') {
             return;
           }
           const attPromise = new Promise(resolve => {
             const attachmentObj = {};
             const attachmentFile = inputElement.files.item(0);
             const attachmentName = attachmentFile.name;
-            const attachmentType = attachmentFile.name.split(".").pop();
+            const attachmentType = attachmentFile.name.split('.').pop();
             attachmentObj.description = attachmentName;
             attachmentObj.type = attachmentType;
             // new Response(file) was added for Safari compatibility
@@ -203,43 +200,42 @@ const planAttachment = (() => {
               resolve();
             });
           });
-    
+
           attachmentProms.push(attPromise);
         });
-    
+
         await Promise.all(attachmentProms);
         await saveAttachmentsToDB();
       }
       //===========================================
       //===========================================
-        
-      
+
       const popup = POPUP.build({
         header: this.header,
-        id: "planAttachmentPopup"
+        id: 'planAttachmentPopup',
       });
-  
+
       const addAttachmentBtn = button.build({
-        text: "Add Attachment",
-        style: "secondary",
-        type: "text",
-        icon: "add",
-        callback: () => addNewAttachment()
+        text: 'Add Attachment',
+        style: 'secondary',
+        type: 'text',
+        icon: 'add',
+        callback: () => addNewAttachment(),
       });
-      addAttachmentBtn.type = "button";
+      addAttachmentBtn.type = 'button';
 
       const removeAttachmentBtn = button.build({
-        style: "secondary",
-        type: "text",
-        icon: "delete"
+        style: 'secondary',
+        type: 'text',
+        icon: 'delete',
       });
-      removeAttachmentBtn.type = "button";
+      removeAttachmentBtn.type = 'button';
 
       const saveBtn = button.build({
-        id: "attachmentSave",
-        text: "Save",
-        style: "secondary",
-        type: "contained",
+        id: 'attachmentSave',
+        text: 'Save',
+        style: 'secondary',
+        type: 'contained',
         callback: async () => {
           try {
             popup.style.display = 'none';
@@ -250,7 +246,7 @@ const planAttachment = (() => {
             setTimeout(() => {
               successfulSave.hide(false);
               // popup.remove();
-              POPUP.hide(popup)
+              POPUP.hide(popup);
             }, 2000);
           } catch (error) {
             pendingSave.reject('Error saving attachment changes');
@@ -258,42 +254,42 @@ const planAttachment = (() => {
               failSave.hide(false);
               popup.style.removeProperty('display');
             }, 2000);
-            console.error(error)
+            console.error(error);
           }
-        }
+        },
       });
       const cancelBtn = button.build({
-        id: "attachmentCancel",
-        text: ro ? "close":"cancel",
-        style: "secondary",
-        type: "outlined",
+        id: 'attachmentCancel',
+        text: ro ? 'close' : 'cancel',
+        style: 'secondary',
+        type: 'outlined',
         callback: () => {
           POPUP.hide(popup);
-        }
+        },
       });
-      const reviewAttachmentList = document.createElement("div");
-      reviewAttachmentList.classList.add("reviewAttachmentList");
-      const newAttachmentList = document.createElement("div");
-      newAttachmentList.classList.add("newAttachmentList");
-      const newAttachmentsHeader = document.createElement("h5");
-      newAttachmentsHeader.innerText = "Attachments to be added:";
+      const reviewAttachmentList = document.createElement('div');
+      reviewAttachmentList.classList.add('reviewAttachmentList');
+      const newAttachmentList = document.createElement('div');
+      newAttachmentList.classList.add('newAttachmentList');
+      const newAttachmentsHeader = document.createElement('h5');
+      newAttachmentsHeader.innerText = 'Attachments to be added:';
       newAttachmentList.appendChild(newAttachmentsHeader);
 
-      const attachmentInput = document.createElement("input");
-      attachmentInput.type = "file";
-      attachmentInput.classList.add("input-field__input", "attachmentInput");
-      attachmentInput.addEventListener('change', evt => fileValidation(evt.target))
+      const attachmentInput = document.createElement('input');
+      attachmentInput.type = 'file';
+      attachmentInput.classList.add('input-field__input', 'attachmentInput');
+      attachmentInput.addEventListener('change', evt => fileValidation(evt.target));
 
-      let attachmentContainer = document.createElement("div");
-      attachmentContainer.classList.add("attachmentContainer");
-      const attachmentList = document.createElement("form");
-      attachmentList.setAttribute("id", "attachmentForm");
+      let attachmentContainer = document.createElement('div');
+      attachmentContainer.classList.add('attachmentContainer');
+      const attachmentList = document.createElement('form');
+      attachmentList.setAttribute('id', 'attachmentForm');
       attachmentList.appendChild(attachmentContainer);
       attachmentContainer.appendChild(removeAttachmentBtn);
       attachmentContainer.appendChild(attachmentInput);
 
-      const btnWrap = document.createElement("div");
-      btnWrap.classList.add("btnWrap");
+      const btnWrap = document.createElement('div');
+      btnWrap.classList.add('btnWrap');
 
       popup.appendChild(reviewAttachmentList);
       if (!ro) {
@@ -308,9 +304,8 @@ const planAttachment = (() => {
 
       if (attachmentsForQuestion.length > 0) populateExistingAttachments();
 
-      POPUP.show(popup)
-
-    };
+      POPUP.show(popup);
+    }
 
     /**
      * Gets the attachment button element
@@ -322,9 +317,9 @@ const planAttachment = (() => {
 
     cleanAttachmentLists(attachmentsToAdd, attachmentsToRemove) {
       attachmentsToRemove.forEach(attachmentId => {
-        let list = attachments.get(this.questionId)
-        let newList = list.filter(a => a.attachmentId !== attachmentId)
-        attachments.set(this.questionId, newList)
+        let list = attachments.get(this.questionId);
+        let newList = list.filter(a => a.attachmentId !== attachmentId);
+        attachments.set(this.questionId, newList);
       });
       attachmentsToAdd.forEach(attachment => {
         let list = [];
@@ -332,16 +327,16 @@ const planAttachment = (() => {
           list = attachments.get(this.questionId);
         }
         list.push(attachment);
-        attachments.set(this.questionId, list)
+        attachments.set(this.questionId, list);
       });
       if (attachments.has(this.questionId)) {
-        this.attachmentsForQuestion = attachments.get(this.questionId)
+        this.attachmentsForQuestion = attachments.get(this.questionId);
       } else {
-        this.attachmentsForQuestion = []
+        this.attachmentsForQuestion = [];
       }
 
       const attachmentBtn = document.getElementById(`attach-${this.questionId}`);
-      attachmentBtn.innerText = `ATTACHMENTS (${this.attachmentsForQuestion.length})`
+      attachmentBtn.innerText = `ATTACHMENTS (${this.attachmentsForQuestion.length})`;
 
       if (this.attachmentsForQuestion.length > 0) {
         attachmentBtn.classList.add('hasAttachments');
@@ -357,22 +352,21 @@ const planAttachment = (() => {
     const retData = {
       token: $.session.Token,
       planId: planId,
-      section: ''
-    }
-    const res = await planAjax.getPlanAttachmentsList(retData)
+      section: '',
+    };
+    const res = await planAjax.getPlanAttachmentsList(retData);
     res.forEach(attachment => {
       let attArray = [];
       if (attachments.has(attachment.questionId)) {
         attArray = attachments.get(attachment.questionId);
       }
       attArray.push(attachment);
-      attachments.set(attachment.questionId, attArray)
+      attachments.set(attachment.questionId, attArray);
     });
-  };
-
+  }
 
   return {
     PlanAttachment,
-    getAttachments
-  }
+    getAttachments,
+  };
 })();
