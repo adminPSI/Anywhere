cc6096b604af9b4f83ed8d454f0392eba6006aad
Nitin <Nitin@NITIN>
2023-03-07 10:22:11 -0500

    #98500 : ANY-CF:  Add New Entry form : done #98676 : ANY-CF:  Edit

--- Gargolye/custom/src/js/modules/ConsumerFinances/ConsumerFinancesAjax.js
+++ Gargolye/custom/src/js/modules/ConsumerFinances/ConsumerFinancesAjax.js
@@ -62,7 +62,203 @@ var ConsumerFinancesAjax = (function () {
                 data: JSON.stringify({
                     token: $.session.Token,
 
+
                 }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function getPayeesAsync() {
+        try {
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/getPayees/',
+                data: JSON.stringify({
+                    token: $.session.Token,
+                    UserId: $.session.UserId,
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function getCategoriesAsync(CategoryID) {
+        try {
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/getCatogories/',
+                data: JSON.stringify({
+                    token: $.session.Token,
+                    categoryID: CategoryID,
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function getSubCategoriesAsync(CategoryID) {
+        try {
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/getSubCatogories/',
+                data: JSON.stringify({
+                    token: $.session.Token,
+                    categoryID: CategoryID,
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function insertPayeeAsync(
+        payeeName,
+        payeeaddress1,
+        payeeaddress2,
+        payeecity,
+        payeestate,
+        payeezipcode
+    ) {
+        try {
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/insertPayee/',
+                data: JSON.stringify({
+                    token: $.session.Token,
+                    payeeName: payeeName,
+                    address1: payeeaddress1,
+                    address2: payeeaddress2,
+                    city: payeecity,
+                    state: payeestate,
+                    zipcode: payeezipcode,
+                    userId: $.session.UserId
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function insertAccountAsync(
+        date, amount, amountType, AccountID, payee, CategoryID, subCategory, checkNo, description, attachmentType, attachment, receipt, BtnName, regId
+    ) {
+        try {
+            debugger; 
+            var binary = '';
+            var bytes = new Uint8Array(attachment);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            let abString = window.btoa(binary);
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/insertAccount/',
+                data: JSON.stringify({
+                    token: $.session.Token,
+                    date: date,
+                    amount: amount,
+                    amountType: amountType,
+                    account: AccountID,
+                    payee: payee,                   
+                    category: CategoryID,
+                    subCategory: subCategory,
+                    checkNo: checkNo,
+                    description: description,
+                    attachmentType: attachmentType,
+                    attachment: abString,
+                    receipt: receipt,  
+                    userId: $.session.UserId,
+                    eventType: BtnName,
+                    regId: regId
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+            return result;
+        } catch (error) {
+            throw new Error(error.responseText);
+        }
+    }
+
+    async function getAccountEntriesByIDAsync(registerId) {
+        try {
+            const result = await $.ajax({
+                type: 'POST',
+                url:
+                    $.webServer.protocol +
+                    '://' +
+                    $.webServer.address +
+                    ':' +
+                    $.webServer.port +
+                    '/' +
+                    $.webServer.serviceName +
+                    '/getAccountEntriesById/',
+                data:
+                    '{"token":"' +
+                    $.session.Token +
+                    '", "registerId":"' +
+                    registerId +
+                    '"}',
                 contentType: 'application/json; charset=utf-8',
                 dataType: 'json',
             });
@@ -75,6 +271,13 @@ var ConsumerFinancesAjax = (function () {
 
     return {
         getAccountTransectionEntriesAsync,
-        getActiveAccountAsync,     
+        getActiveAccountAsync,
+        getPayeesAsync,
+        getCategoriesAsync,
+        getSubCategoriesAsync,
+        insertPayeeAsync,
+        insertAccountAsync,
+        getAccountEntriesByIDAsync,
+
     };
 })();
--- Gargolye/service/Data/ConsumerFinances/ConsumerFinancesWorker.cs
+++ Gargolye/service/Data/ConsumerFinances/ConsumerFinancesWorker.cs
@@ -7,6 +7,10 @@ using System.Web.Script.Serialization;
 using System.ServiceModel.Web;
 using System.IO;
 using Anywhere.Data;
+using static Anywhere.service.Data.OODWorker;
+using OneSpanSign.Sdk;
+using static System.Windows.Forms.AxHost;
+using System.Reflection.Emit;
 
 namespace Anywhere.service.Data.ConsumerFinances
 {
@@ -30,6 +34,7 @@ namespace Anywhere.service.Data.ConsumerFinances
             public string payee { get; set; }
             [DataMember(Order = 4)]
             public string category { get; set; }
+
             [DataMember(Order = 5)]
             public string amount { get; set; }
             [DataMember(Order = 6)]
@@ -40,9 +45,20 @@ namespace Anywhere.service.Data.ConsumerFinances
             public string enteredby { get; set; }
             [DataMember(Order = 9)]
             public string AttachmentsID { get; set; }
+            [DataMember(Order = 10)]
+            public string subCategory { get; set; }
+            [DataMember(Order = 11)]
+            public string accountType { get; set; }
 
+            [DataMember(Order = 12)]
+            public string description { get; set; }
+            [DataMember(Order = 13)]
+            public string receipt { get; set; }
 
-        }     
+            [DataMember(Order = 13)]
+            public string accountID { get; set; }
+
+    }
 
         [DataContract]
         public class ActiveAccount
@@ -53,14 +69,73 @@ namespace Anywhere.service.Data.ConsumerFinances
             public string accountName { get; set; }
 
         }
-      
+
+        [DataContract]
+        public class Payees
+        {
+            [DataMember(Order = 0)]
+            public string CategoryID { get; set; }
+            [DataMember(Order = 1)]
+            public string Description { get; set; }
+
+        }
+
+        [DataContract]
+        public class Category
+        {
+            [DataMember(Order = 0)]
+            public string CategoryID { get; set; }
+            [DataMember(Order = 1)]
+            public string CategoryDescription { get; set; }
+
+        }
+
+        [DataContract]
+        public class SubCategory
+        {
+            [DataMember(Order = 0)]
+            public string CategoryID { get; set; }
+            [DataMember(Order = 1)]
+            public string SubCategoryDescription { get; set; }
+
+        }
+
+        [DataContract]
+        public class ActivePayee
+        {
+            [DataMember(Order = 0)]
+            public string RegionID { get; set; }
+            [DataMember(Order = 1)]
+            public string Description { get; set; }
+            [DataMember(Order = 2)]
+            public string address1 { get; set; }
+            [DataMember(Order = 3)]
+            public string address2 { get; set; }
+            [DataMember(Order = 4)]
+            public string city { get; set; }
+            [DataMember(Order = 5)]
+            public string state { get; set; }
+            [DataMember(Order = 6)]
+            public string zipcode { get; set; }
+
+
+        }
+
+        [DataContract]
+        public class AccountRegister
+        {
+            [DataMember(Order = 0)]
+            public string accountID { get; set; }
+
+        }
+
         public ConsumerFinancesEntry[] getAccountTransectionEntries(string token, string consumerIds, string activityStartDate, string activityEndDate, string accountName, string payee, string category, string amount, string checkNo, string balance, string enteredBy)
         {
             using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
             {
                 try
                 {
-                    ConsumerFinancesEntry[] entries = js.Deserialize<ConsumerFinancesEntry[]>(Odg.getAccountTransectionEntries(token, consumerIds, activityStartDate , activityEndDate, accountName, payee, category, amount, checkNo, balance, enteredBy, transaction));
+                    ConsumerFinancesEntry[] entries = js.Deserialize<ConsumerFinancesEntry[]>(Odg.getAccountTransectionEntries(token, consumerIds, activityStartDate, activityEndDate, accountName, payee, category, amount, checkNo, balance, enteredBy, transaction));
 
                     return entries;
 
@@ -92,6 +167,161 @@ namespace Anywhere.service.Data.ConsumerFinances
                 }
             }
         }
-     
+
+        public Payees[] getPayees(string token, string UserId)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    js.MaxJsonLength = Int32.MaxValue;
+                    if (!wfdg.validateToken(token, transaction)) throw new Exception("invalid session token");
+                    Payees[] payees = js.Deserialize<Payees[]>(Odg.getPayees(transaction, UserId));
+                    return payees;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        public Category[] getCatogories(string token, string categoryID)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    js.MaxJsonLength = Int32.MaxValue;
+                    if (!wfdg.validateToken(token, transaction)) throw new Exception("invalid session token");
+                    Category[] category = js.Deserialize<Category[]>(Odg.getCatogories(transaction, categoryID));
+                    return category;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        public SubCategory[] getSubCatogories(string token, string categoryID)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    js.MaxJsonLength = Int32.MaxValue;
+                    if (!wfdg.validateToken(token, transaction)) throw new Exception("invalid session token");
+                    SubCategory[] subCategory = js.Deserialize<SubCategory[]>(Odg.getSubCatogories(transaction, categoryID));
+                    return subCategory;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        public ActivePayee insertPayee(string token, string payeeName, string address1, string address2, string city, string state, string zipcode, string userId)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    if (!wfdg.validateToken(token, transaction)) throw new Exception("invalid session token");
+
+                    if (payeeName == null) throw new Exception("employerName is required");
+                    if (address1 == null) throw new Exception("address1 is required");
+                    if (city == null) throw new Exception("city is required");
+                    if (state == null) throw new Exception("state is required");
+                    if (zipcode == null) throw new Exception("zipcode is required");
+                    if (userId == null) throw new Exception("userId is required");
+
+                    // insert document
+                    String RegionID = Odg.insertPayee(payeeName, address1, address2, city, state, zipcode, userId, transaction);
+
+                    ActivePayee payee = new ActivePayee();
+                    payee.RegionID = RegionID;
+
+                    return payee;
+
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+
+        public AccountRegister insertAccount(string token, string date, string amount, string amountType, string account, string payee, string category, string subCategory, string checkNo, string description, string attachmentType, string attachment, string receipt, string userId, string eventType, string regId)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    AccountRegister acount = new AccountRegister();
+                    if (!wfdg.validateToken(token, transaction)) throw new Exception("invalid session token");
+
+                    if (date == null) throw new Exception("date is required");
+                    if (amount == null) throw new Exception("amount is required");
+                    if (payee == null) throw new Exception("payee is required");
+                    if (category == null) throw new Exception("category is required");
+                    if (userId == null) throw new Exception("userId is required");
+
+                    String attachmentId = "0";
+                    if (attachmentType != null && attachment != null)
+                    {
+                        attachmentId = wfdg.insertAttachment(attachmentType, attachment, transaction);
+                    }
+                    String AccountID;
+
+                    if (eventType == "UPDATE")
+                    {
+                        AccountID = Odg.updateAccount(token, date, amount, amountType, account, payee, category, subCategory, checkNo, description, attachmentId, receipt, userId, transaction, regId);
+                    }
+                    else
+                    {
+                        AccountID = Odg.insertAccount(token, date, amount, amountType, account, payee, category, subCategory, checkNo, description, attachmentId, receipt, userId, transaction);
+                    }
+
+                    acount.accountID = AccountID;
+
+                    return acount;
+
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        public ConsumerFinancesEntry[] getAccountEntriesById(string token, string registerId)
+        {
+            using (DistributedTransaction transaction = new DistributedTransaction(DbHelper.ConnectionString))
+            {
+                try
+                {
+                    ConsumerFinancesEntry[] accountEntries = js.Deserialize<ConsumerFinancesEntry[]>(Odg.getAccountEntriesById(token, registerId, transaction));
+
+                    return accountEntries;
+
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new WebFaultException<string>(ex.Message, System.Net.HttpStatusCode.BadRequest);
+                }
+            }
+
+        }
+
+
     }
 }
