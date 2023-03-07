f78ecbd734347b07dd5f4a62643c1d65d577ea08
Erick Bey <erick.bey@primarysolutions.net>
2023-03-06 22:31:44 -0500

    67222 -- One Span Integration

--- Gargolye/custom/src/js/modules/OneSpan/oneSpanAjax.js
+++ Gargolye/custom/src/js/modules/OneSpan/oneSpanAjax.js
@@ -1,57 +1,29 @@
 ﻿var oneSpanAjax = (function() {
 	'use-strict';
-	//Incident Save
-	//Send incident notification
-    function sendPackage() {
-        const retrieveData = {
-            test: "test:",
-        };
-		$.ajax({
-			type: 'POST',
-            url: 
-                $.webServer.protocol + 
-                '://' + $.webServer.address + 
-                ':' + $.webServer.port + 
-                '/' + 
-                $.webServer.serviceName + 
-                '/sendOneSpanPackage/',
-            data: JSON.stringify(retrieveData),
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			success: function(response, status, xhr) {
-                var res = response.sendPackageResult;
-                return res;
-			},
-			error: function(xhr, status, error) {
-				//alert("Error\n-----\n" + xhr.status + '\n' + xhr.responseText);
-			},
-		});
-	}
 
-    function oneSpanBuildSigners(retrieveData) {
+    async function oneSpanBuildSigners(retrieveData) {
         //token, assessmentID, userID, versionID, extraSpace, isp
-        $.ajax({
-            type: 'POST',
-            url: 
-                $.webServer.protocol + 
-                '://' + 
-                $.webServer.address + 
-                ':' + 
-                $.webServer.port + 
-                '/' + 
-                $.webServer.serviceName + 
-                '/oneSpanBuildSigners/',
-            data: JSON.stringify(retrieveData),
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: function (response, status, xhr) {
-                var res = response.oneSpanBuildSignersResult;
-                return res;
-            },
-            error: function (xhr, status, error) {
-                // alert("Error\n-----\n" + xhr.status + '\n' + xhr.responseText);
-            },
-        });
+        try {
+            const data = await $.ajax({
+                type: 'POST',
+                url: 
+                    $.webServer.protocol + 
+                    '://' + 
+                    $.webServer.address + 
+                    ':' + 
+                    $.webServer.port + 
+                    '/' + 
+                    $.webServer.serviceName + 
+                    '/oneSpanBuildSigners/',
+                data: JSON.stringify(retrieveData),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+            });
+
+            return await data.oneSpanBuildSignersResult;
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     async function oneSpanGetSignedDocuments(retrieveData) {
@@ -73,7 +45,7 @@
                 dataType: 'json',
         });
 
-        return data.oneSpanGetSignedDocumentsResult;
+        return  await data.oneSpanGetSignedDocumentsResult;
         
      } catch (error) {
             console.log(error)
@@ -107,7 +79,7 @@
     }
 
 	return {
-        sendPackage,
+
         oneSpanBuildSigners,
         oneSpanGetSignedDocuments,
         oneSpanCheckDocumentStatus
