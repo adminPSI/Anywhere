9f754238e41840fd84e64ade2e8186c77bdc5f91
Primary Solutions <mike.taft@primarysolutions.net>
2023-03-09 15:25:41 -0500

    added keytimes to procedure to validate entries properly

--- Gargolye/custom/gulpfile.js
+++ Gargolye/custom/gulpfile.js
@@ -19,7 +19,8 @@ const paths = {
     base: './src/',
     assets: './src/assets/**',
     html: './src/*.html',
-    js: './src/js/**/*.js',
+    js: ['./src/js/**/*.js', '!./src/js/UI/**/*.js'],
+    jsui: ['./src/js/**/*.js'],
     lib: './src/lib/**',
     scss: ['./src/scss/anywhere.scss', './src/scss/login.scss', './src/scss/infalAnywhere.scss'],
   },
@@ -61,15 +62,32 @@ function javaScript() {
   return src(paths.src.js)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['@babel/preset-env'],
-      sourceType: 'script',
-    }))
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+        sourceType: 'script',
+      }),
+    )
     .pipe(concat('bundle.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(size())
     .pipe(dest(paths.dist.js));
-};
+}
+function javaScriptWithUI() {
+  return src(paths.src.jsui)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+        sourceType: 'script',
+      }),
+    )
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(size())
+    .pipe(dest(paths.dist.js));
+}
 // Other
 function copyAssets() {
   return src(paths.src.assets, { base: paths.src.base }).pipe(dest(paths.dist.base));
@@ -84,8 +102,14 @@ function copyLib() {
 // EXPORTS
 //===========================================================
 exports.build = series(parallel(copyAssets, copyHTML, copyLib), javaScript, styles);
+exports.startUI = () => {
+  fancylog(`Watching....`);
+  watch(paths.src.jsui, javaScriptWithUI);
+  watch(paths.src.base + '**/*.scss', styles);
+  watch(paths.src.html, copyHTML);
+};
 exports.default = () => {
-  fancylog(`Watching....`)
+  fancylog(`Watching....`);
   watch(paths.src.js, javaScript);
   watch(paths.src.base + '**/*.scss', styles);
   watch(paths.src.html, copyHTML);
