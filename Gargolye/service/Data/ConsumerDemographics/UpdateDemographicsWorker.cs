a82726d7b99b6359a5866986ee5f80f77b86542f
mike.taft <mike.taft@primarysolution.net>
2023-03-23 07:32:51 -0400

    demographics

--- Gargolye/service/Data/ConsumerDemographics/UpdateDemographicsWorker.cs
+++ Gargolye/service/Data/ConsumerDemographics/UpdateDemographicsWorker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Anywhere.service.Data.ConsumerDemographics
@@ -11,137 +12,139 @@ namespace Anywhere.service.Data.ConsumerDemographics
         public string updateDemographicsRecord(string consumerId, string field, string newValue, string applicationName)
         {
             string query = "";
-            if (field.Equals("address1")){
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            if (field.Equals("addressOne")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set resident_address = " + newValue + " where id = " + consumerId; 
+                    sb.AppendFormat("update dba.people set resident_address = {0} where people.id = {1}; commit; ", "'"+newValue+"'", consumerId); 
                 }
                 else {
-                    query = "update consumers set address1 = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set address1 = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
-            if (field.Equals("address2")){
+            if (field.Equals("addressTwo")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set resident_address_2 = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set resident_address_2 = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set address2 = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set address2 = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("city")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set resident_city = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set resident_city = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set city = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set city = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("state")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set resident_state = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set resident_state = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set state = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set state = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("zip")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set resident_zip = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set resident_zip = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set zipcode = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set zipcode = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("primaryPhone")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set primary_phone = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set primary_phone = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set phone = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set phone = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("secondaryPhone")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set secondary_phone = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set secondary_phone = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set phone_2 = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set phone_2 = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("cellPhone")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set cellular = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set Cellular = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    //query = "update consumers set address1 = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set Cellular = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("email")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set e_mail = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set e_mail = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set email_address = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set email_address = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
-            if (field.Equals("dob")){
+            if (field.Equals("dateOfBirth")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update people set date_of_birth = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.people set date_of_birth = {0} where people.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set date_of_birth = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set date_of_birth = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("medicaidNumber")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update consumer_info set Medicaid_Number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumer_info set Medicaid_Number = {0} where consumer_info.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set medicaid_number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set medicaid_number = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("medicareNumber")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update consumer_info set Medicare_Number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumer_info set Medicare_Number = {0} where consumer_info.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set Medicare_Number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set Medicare_Number = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
             if (field.Equals("residentNumber")){
                 if (applicationName.ToUpper() == "GATEKEEPER")
                 {
-                    query = "update consumer_info set Resident_Number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumer_info set Resident_Number = {0} where consumer_info.id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
                 else
                 {
-                    query = "update consumers set resident_Number = " + newValue + " where id = " + consumerId;
+                    sb.AppendFormat("update dba.consumers set resident_Number = {0} where consumers.consumer_id = {1}; commit; ", "'" + newValue + "'", consumerId);
                 }
             }
 
-            long ret = di.UpdateRecord(query);
+            long ret = di.UpdateRecord(sb.ToString());
             if(ret.ToString() == "-999")
             {
                 return "fail";
