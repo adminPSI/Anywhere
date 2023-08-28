0ef251497e70eb095088abad7de307455a96c961
mike.taft <mike.taft@primarysolution.net>
2023-08-28 11:27:33 -0400

    prevPlanId added as parameter

--- Gargolye/service/Anywhere.svc.cs
+++ Gargolye/service/Anywhere.svc.cs
@@ -1769,9 +1769,9 @@ namespace Anywhere
             return aPW.insertConsumerPlanAnnual(token, consumerId, planYearStart, reviewDate, salesForceCaseManagerId);
         }
 
-        public string switchPlanType(string token, string consumerPlanId, string planType, string planYearStart, string planYearEnd, string effectiveStartDate, string effectiveEndDate, string reviewDate)
+        public string switchPlanType(string token, string consumerPlanId, string planType, string planYearStart, string planYearEnd, string effectiveStartDate, string effectiveEndDate, string reviewDate, string prevPlanId)
         {
-            return aAW.switchPlanType(token, consumerPlanId, planType, planYearStart, planYearEnd, effectiveStartDate, effectiveEndDate, reviewDate);
+            return aAW.switchPlanType(token, consumerPlanId, planType, planYearStart, planYearEnd, effectiveStartDate, effectiveEndDate, reviewDate, prevPlanId);
         }
 
         public int updateConsumerNameInfo(string token, string consumerId, string firstName, string lastName, string middleName, string nickName)
--- Gargolye/service/Data/AnywhereAssessmentWorker.cs
+++ Gargolye/service/Data/AnywhereAssessmentWorker.cs
@@ -165,9 +165,9 @@ namespace Anywhere.service.Data
             return assessmentObj;
         }
 
-        public string switchPlanType(string token, string consumerPlanId, string planType, string planYearStart, string planYearEnd, string effectiveStartDate, string effectiveEndDate, string reviewDate)
+        public string switchPlanType(string token, string consumerPlanId, string planType, string planYearStart, string planYearEnd, string effectiveStartDate, string effectiveEndDate, string reviewDate, string prevPlanId)
         {
-            string resultString = adg.switchPlanType( token,  consumerPlanId,  planType, planYearStart, planYearEnd,  effectiveStartDate,  effectiveEndDate,  reviewDate);
+            string resultString = adg.switchPlanType( token,  consumerPlanId,  planType, planYearStart, planYearEnd,  effectiveStartDate,  effectiveEndDate,  reviewDate, prevPlanId);
             return resultString;
         }
 
