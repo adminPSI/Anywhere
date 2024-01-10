2d33aab69f294315f451acd4db824ab1b602c461
Primary Solutions <mike.taft@primarysolutions.net>
2024-01-10 15:21:05 -0500

    waiting list setup

---
+++ Gargolye/custom/src/js/modules/WaitingList/waitingList.js
@@ -0,0 +1,59 @@
+const WaitingList = (() => {
+  //--------------------------
+  // SESSION DATA
+  //--------------------------
+  //--------------------------
+  // PERMISSIONS
+  //--------------------------
+  let isReadOnly;
+  //--------------------------
+  // DOM
+  //--------------------------
+  let moduleWrap;
+  //--------------------------
+  // UI INSTANCES
+  //--------------------------
+  let rosterPicker;
+  let wlForms;
+
+  // ROSTER
+  //--------------------------------------------------
+  async function onConsumerSelect(data) {
+    selectedConsumer = data;
+  }
+
+  // MAIN
+  //--------------------------------------------------
+  function attachEvents() {}
+  async function populatePage() {}
+  async function loadPage() {}
+  function loadPageSkeleton() {
+    // prep actioncenter
+    _DOM.ACTIONCENTER.innerHTML = '';
+    _DOM.ACTIONCENTER.setAttribute('data-UI', true);
+    _DOM.setActiveModuleAttribute('waitingList');
+
+    // build DOM skeleton
+    moduleWrap = _DOM.createElement('div', { class: 'waitingListModule' });
+
+    _DOM.ACTIONCENTER.appendChild(moduleWrap);
+  }
+
+  // INIT (data & defaults)
+  //--------------------------------------------------
+  function initComponents() {
+    // Roster Picker
+    rosterPicker = new RosterPicker({
+      allowMultiSelect: false,
+      consumerRequired: true,
+    });
+
+    wlForms = {};
+  }
+
+  function init() {}
+
+  return {
+    init,
+  };
+})();
---
+++ Gargolye/custom/src/js/modules/WaitingList/waitingListData.js
@@ -0,0 +1,16 @@
+(function (global, factory) {
+  global.WaitingListData = factory();
+})(this, function () {
+  //=======================================
+  // MAIN LIB
+  //---------------------------------------
+  /**
+   * Constructor function
+   *
+   * @constructor
+   * @returns {WaitingListData}
+   */
+  function WaitingListData() {}
+
+  return WaitingListData;
+});
