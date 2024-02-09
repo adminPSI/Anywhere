c5f7ec9af31c7b0d06d1f149b8f2dc2269e8d6b8
Primary Solutions <mike.taft@primarysolutions.net>
2024-02-09 10:57:23 -0500

    clipboard

--- Gargolye/custom/src/js/modules/CaseNotes/clipboard.js
+++ Gargolye/custom/src/js/modules/CaseNotes/clipboard.js
@@ -1,1 +1,36 @@
 // when calling [input].setValue() we need to run insert/update assessment anwser
+// lock form down until they select a consumer
+
+// function initFormInfo() {
+//   return {
+//     adultDayEmployment: { enabled: false, dbtable: 'WLA_Require_Waiver_Fundings' },
+//     childProtectionAgency: { enabled: false, dbtable: 'WLA_Child_Protection_Agencies' },
+//     conditions: { enabled: true, dbtable: 'WLA_Conditions' },
+//     currentAvailableServices: { enabled: false, dbtable: 'WLA_Active_Services' },
+//     currentNeeds: { enabled: false, dbtable: 'WLA_Unmet_Needs' },
+//     dischargePlan: { enabled: false, dbtable: 'WLA_Discharge_Plans' },
+//     icfDischarge: { enabled: false, dbtable: 'WLA_ICF_Discharges' },
+//     intermittentSupports: { enabled: false, dbtable: 'WLA_Intermitent_Supports' },
+//     immediateNeeds: { enabled: false, dbtable: 'WLA_Immediate_Needs' },
+//     primaryCaregiver: { enabled: true, dbtable: 'WLA_Primary_Caregivers' },
+//     riskMitigation: { enabled: false, dbtable: 'WLA_Risk_Mitigations' },
+//     waitingListInfo: { enabled: true, dbtable: 'WLA_Waiting_List_Information' },
+//     waiverEnrollment: { enabled: false, dbtable: 'WLA_Waiver_Enrollments' },
+//     needs: {
+//       enabled: false,
+//       behavioral: { dbtable: 'WLA_Risks' },
+//       physical: { dbtable: 'WLA_Physical_Needs' },
+//       medical: { dbtable: 'WLA_Medical_Needs' },
+//       other: { dbtable: 'WLA_Needs' },
+//     },
+//     //------
+//     conclusion: {
+//       enabled: false,
+//     },
+//   };
+// }
+
+// wlFormInfo.needs.behavioral.id = '';
+// wlFormInfo.needs.physical.id = '';
+// wlFormInfo.needs.medical.id = '';
+// wlFormInfo.needs.other.id = '';
