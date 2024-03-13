67f2182abafe0c29360ccedd39439e4cf3b66b37
Nitin <Nitin@NITIN>
2024-03-13 03:02:08 -0400

    116124 : ticket new requirement implement filter functionality

--- Gargolye/custom/src/js/modules/Dashboard/IncidentTracking/incidentTracking.js
+++ Gargolye/custom/src/js/modules/Dashboard/IncidentTracking/incidentTracking.js
@@ -1,93 +1,189 @@
 var incidentTrackingWidget = (function () {
-  var tableOptions = {
-    plain: true,
-    columnHeadings: ['Consumer Involved', 'Date of Incident', 'Category / Subcategory'],
-    tableId: 'incidentTrackingWidgetTable',
-  };
 
-  function populateIncidentTrackingWidget(res) {
-    var widget = document.getElementById('incidenttrackingwidget');
-    if (!widget) return;
-    var widgetBody = widget.querySelector('.widget__body');
+    let widget;
+    let widgetBody;
+    let defaultShow;
+    let tempShow;
+    let incidentTrackingWidgetObj = []; 
 
-    var itTable = table.build(tableOptions);
-    widgetBody.innerHTML = '';
-    widgetBody.appendChild(itTable);
+    function buildFilterPopup() {
+        var widgetFilter = widget.querySelector(".widget__filters");
+        if (widgetFilter) return;
+        filterPopup = dashboard.buildFilterPopup();
 
-    var incidents = {};
-    res.forEach(r => {
-      if (!incidents[r.incidentId]) {
-        incidents[r.incidentId] = r;
-      } else {
-        const dupName = incidents[r.incidentId].consumerName.includes(r.consumerName);
-        if (!dupName) {
-          incidents[r.incidentId].consumerName += `, ${r.consumerName}`;
+        applyFiltersBtn = button.build({
+            text: "Apply",
+            style: "secondary",
+            type: "contained"
+        });
+        cancelFilterBtn = button.build({
+            text: "Cancel",
+            style: "secondary",
+            type: "outlined"
+        });
+        showDropdown = dropdown.build({
+            dropdownId: "showDropdown",
+            label: "Show",
+            style: "secondary"
+        });
+
+        var btnWrap = document.createElement("div");
+        btnWrap.classList.add("btnWrap");
+
+        btnWrap.appendChild(applyFiltersBtn);
+        btnWrap.appendChild(cancelFilterBtn);
+        filterPopup.appendChild(showDropdown);
+        filterPopup.appendChild(btnWrap);
+        widget.insertBefore(filterPopup, widgetBody);
+    }
+
+    function eventSetup() {
+        applyFiltersBtn.addEventListener("click", event => {
+            filterPopup.classList.remove("visible");
+            overlay.hide();
+            bodyScrollLock.enableBodyScroll(filterPopup);
+            if (defaultShow === tempShow || !tempShow)
+                return;
+            defaultShow = tempShow;
+            UTIL.LS.setStorage('dash_incidentTrackingShow', defaultShow);
+            populateIncidentTrackingWidget(incidentTrackingWidgetObj);
+        });
+        cancelFilterBtn.addEventListener("click", event => {
+            filterPopup.classList.remove("visible");
+            overlay.hide();
+            bodyScrollLock.enableBodyScroll(filterPopup);
+        });
+        showDropdown.addEventListener("change", event => {
+            tempShow = event.target[event.target.selectedIndex].value;
+        });
+    }
+
+    function getData() {
+        let showValue = UTIL.LS.getStorage('dash_incidentTrackingShow');
+        if (showValue === undefined) {
+            showValue = "%";
+            UTIL.LS.setStorage('dash_incidentTrackingShow', '%')
         }
-        incidents[r.incidentId].viewedBy += `, ${r.viewedBy}`;
-      }
-    });
+        defaultShow = showValue;
 
-    var keys = Object.keys(incidents);
+        const showDropdownData = ([
+            { id: 1, value: 1, text: 'Unread' },
+            { id: 2, value: 2, text: 'Read' },
+        ]);
+        showDropdownData.unshift({ id: null, value: '%', text: 'ALL' });
+        dropdown.populate("showDropdown", showDropdownData, defaultShow);
+    }
 
-    var data = keys
-      .filter(k => {
-        var obj = incidents[k];
+    var tableOptions = {
+        plain: true,
+        columnHeadings: ['Consumer Involved', 'Date of Incident', 'Category / Subcategory'],
+        tableId: 'incidentTrackingWidgetTable',
+    };
 
-        if ($.session.incidentTrackingViewPerm.length !== 0) {
-          if (
-            obj.description !== '' &&
-            !$.session.incidentTrackingViewPerm.includes(obj.description.toLowerCase())
-          ) {
-            return false;
-          }
-        }
+    function populateIncidentTrackingWidget(res) {
+        var itTable = table.build(tableOptions);
+        widgetBody.innerHTML = '';
+        widgetBody.appendChild(itTable);
 
-        return true;
-      })
-      .map(r => {
-        var obj = incidents[r];
+        var incidents = {};
+        res.forEach(r => {
+            if (!incidents[r.incidentId]) {
+                incidents[r.incidentId] = r;
+            } else {
+                const dupName = incidents[r.incidentId].consumerName.includes(r.consumerName);
+                if (!dupName) {
+                    incidents[r.incidentId].consumerName += `, ${r.consumerName}`;
+                }
+                incidents[r.incidentId].viewedBy += `, ${r.viewedBy}`;
+            }
+        });
 
-        var name = obj.consumerName.split(',');
-        name = `${name[1]}, ${name[0]}`;
-        var date = UTIL.abbreviateDateYear(obj.incidentDate.split(' ')[0]);
-        var viewedOn = r.viewedOn ? true : false;
-        var orginUser =
-          obj.originallyEnteredBy.toLowerCase() === $.session.UserId.toLowerCase() ? true : false;
-        var userHasViewed = obj.viewedBy.includes($.session.UserId) ? true : false;
-        var showBold;
+        var keys = Object.keys(incidents);
+        const removeArray = [];
+        var count = 0;
+        var data = keys
+            .filter(k => {
+                var obj = incidents[k];
 
-        if (!orginUser && !userHasViewed) {
-          showBold = true;
-        }
+                if ($.session.incidentTrackingViewPerm.length !== 0) {
+                    if (
+                        obj.description !== '' &&
+                        !$.session.incidentTrackingViewPerm.includes(obj.description.toLowerCase())
+                    ) {
+                        return false;
+                    }
+                }
 
-        return {
-          values: [name, date, obj.incidentCategory],
-          attributes: [{ key: 'data-viewed', value: showBold }],
-          id: obj.incidentId,
-          onClick: async () => {
-            await incidentTrackingAjax.updateIncidentViewByUser({
-              token: $.session.Token,
-              incidentId: obj.incidentId,
-              userId: $.session.UserId,
+                return true;
+            })
+            .map(r => {
+                var obj = incidents[r];
+
+                var name = obj.consumerName.split(',');
+                name = `${name[1]}, ${name[0]}`;
+                var date = UTIL.abbreviateDateYear(obj.incidentDate.split(' ')[0]);
+                var viewedOn = r.viewedOn ? true : false;
+                var orginUser =
+                    obj.originallyEnteredBy.toLowerCase() === $.session.UserId.toLowerCase() ? true : false;
+                var userHasViewed = obj.viewedBy.includes($.session.UserId) ? true : false;
+                var showBold;
+
+                if (!orginUser && !userHasViewed) {
+                    showBold = true;
+                } 
+                if (defaultShow == 2 && showBold) {
+                    removeArray.push(count);
+                }
+                else if (defaultShow == 1 && !showBold) {
+                    removeArray.push(count);
+                }
+                count = count + 1;
+                return {
+                    values: [name, date, obj.incidentCategory],
+                    attributes: [{ key: 'data-viewed', value: showBold }],
+                    id: obj.incidentId,
+                    onClick: async () => {
+                        await incidentTrackingAjax.updateIncidentViewByUser({
+                            token: $.session.Token,
+                            incidentId: obj.incidentId,
+                            userId: $.session.UserId,
+                        });
+
+                        incidentTracking.getDropdownData(() => {
+                            setActiveModuleSectionAttribute('incidentTracking-overview');
+                            UTIL.toggleMenuItemHighlight('incidenttracking');
+                            actioncenter.dataset.activeModule = 'incidenttracking';
+                            reviewIncident.init(obj.incidentId);
+                        });
+                    },
+                };
             });
+        for (let i = removeArray.length - 1; i >= 0; i--)
+            data.splice(removeArray[i], 1);
 
-            incidentTracking.getDropdownData(() => {
-              setActiveModuleSectionAttribute('incidentTracking-overview');
-              UTIL.toggleMenuItemHighlight('incidenttracking');
-              actioncenter.dataset.activeModule = 'incidenttracking';
-              reviewIncident.init(obj.incidentId);
-            });
-          },
-        };
-      });
-    table.populate(itTable, data);
-  }
+        table.populate(itTable, data);
+    }
 
-  function init() {
-    incidentTrackingWidgetAjax.getITDashboardWidgetDataAjax(populateIncidentTrackingWidget);
-  }
+    function init() {
+        widget = document.getElementById('incidenttrackingwidget');
+        if (!widget) return;
+        widgetBody = widget.querySelector('.widget__body');
+        dashboard.appendFilterButton(
+            "incidenttrackingwidget",
+            "incidenttrackingFilterBtn"
+        );
+        defaultShow = '%';
+        buildFilterPopup();
+        eventSetup();
+        getData();
+        incidentTrackingWidgetAjax.getITDashboardWidgetDataAjax(res => {
+            incidentTrackingWidgetObj = res; 
+            populateIncidentTrackingWidget(incidentTrackingWidgetObj);  
+        }); 
+              
+    }
 
-  return {
-    init,
-  };
+    return {
+        init,
+    };
 })();
