31fa7a8dbe18cf44970bc58388f685c018400b7d
Nitin <Nitin@NITIN>
2024-04-08 08:23:35 -0400

    110296 : Old responsible party ID should be show in grid

--- Gargolye/custom/src/js/modules/Workflow/Viewer/workflows.component.js
+++ Gargolye/custom/src/js/modules/Workflow/Viewer/workflows.component.js
@@ -1,15 +1,16 @@
 class WorkflowsComponent {
-    constructor({workflows, people}){
+    constructor({ workflows, people, responsiblePeople }){
         this.workflows = workflows;
         this.people = people;
+        this.responsiblePeople = responsiblePeople;
     }
 
     render() {
-        let {people} = this;
+        let { people, responsiblePeople } = this;
         let workflowsComponent = document.createElement('div');   
         if (this.workflows.length > 0){
             this.workflows.map(w => { 
-                workflowsComponent.appendChild(new WorkflowComponent(w, people).render())                
+                workflowsComponent.appendChild(new WorkflowComponent(w, people, responsiblePeople).render())                
             });    
         } else {
             workflowsComponent.innerHTML = `There are no workflows attached to this record`;
