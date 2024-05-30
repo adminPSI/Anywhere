eaeef3758157d7cb1ef7792001015708ecbaa34d
Erick Bey <erick.bey@primarysolutions.net>
2024-05-30 11:13:18 -0400

    progress notes new note on roster fix

--- Gargolye/custom/src/js/modules/Roster/info/consumerInfo.js
+++ Gargolye/custom/src/js/modules/Roster/info/consumerInfo.js
@@ -280,7 +280,10 @@ var consumerInfo = (function () {
 
         setTimeout(function () {
             // shrink currently visible sub section
-            currentlyVisibleSubSection.classList.add('hidden');
+            // the viewAllNotesSection can't be hidden or else add new note section doesn't work
+            if (!currentlyVisibleSubSection.classList.contains('viewAllNotesSection')) {
+                currentlyVisibleSubSection.classList.add('hidden');
+            }
             // show target sub section
             targetSubSection.classList.add('visible');
         }, 200);
