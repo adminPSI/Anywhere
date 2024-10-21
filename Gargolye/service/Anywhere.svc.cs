bbe1ec435f8e4f275ab3dc34ebe78cd9fc884148
Erick Bey <erick.bey@primarysolutions.net>
2024-10-21 14:17:03 -0400

    form 10 updates on person completing reporrt and the invoice number function

--- Gargolye/service/Anywhere.svc.cs
+++ Gargolye/service/Anywhere.svc.cs
@@ -3074,6 +3074,7 @@ namespace Anywhere
             string startDate;
             string endDate;
             string userId;
+            string loggedInUserPersonId;
 
             StreamReader reader = new StreamReader(testInput);
             string fullInput = reader.ReadToEnd();
@@ -3084,8 +3085,9 @@ namespace Anywhere
             serviceCodeId = System.Text.RegularExpressions.Regex.Split(System.Text.RegularExpressions.Regex.Split(fullInput, "&")[4], "=")[1];
             startDate = System.Text.RegularExpressions.Regex.Split(System.Text.RegularExpressions.Regex.Split(fullInput, "&")[5], "=")[1];
             endDate = System.Text.RegularExpressions.Regex.Split(System.Text.RegularExpressions.Regex.Split(fullInput, "&")[6], "=")[1];
+            loggedInUserPersonId = System.Text.RegularExpressions.Regex.Split(System.Text.RegularExpressions.Regex.Split(fullInput, "&")[7], "=")[1];
 
-            return OODfw.generateForm10(token, referenceNumber, 22, peopleId, startDate, endDate, userId);
+            return OODfw.generateForm10(token, referenceNumber, 22, peopleId, startDate, endDate, userId, loggedInUserPersonId);
 
         }
 
