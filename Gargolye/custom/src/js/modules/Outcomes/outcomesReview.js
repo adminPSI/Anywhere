71991dcbd8cefa3f486bba592064468bb2391f7f
Primary Solutions <mike.taft@primarysolutions.net>
2024-10-25 15:04:55 -0400

    outcomes review

--- Gargolye/custom/src/js/modules/Outcomes/outcomesReview.js
+++ Gargolye/custom/src/js/modules/Outcomes/outcomesReview.js
@@ -1272,7 +1272,7 @@ const outcomesReview = (function () {
       const percentData = UTIL.parseXml(d.percentData);
       const bottomNum = percentData.getElementsByTagName('bottomnumber')[0].textContent;
       const topNumb = percentData.getElementsByTagName('topnumber')[0].textContent;
-      let percent = bottomNum === '0' ? 0 : parseInt(topNumb)/parseInt(bottomNum);
+      let percent = bottomNum === '0' || !bottomNum || topNumb === '0' || !topNumb ? 0 : parseInt(topNumb)/parseInt(bottomNum);
       percent = percent > 0 ? percent * 100 : 0;
   
       objIdSet.add(objID);
